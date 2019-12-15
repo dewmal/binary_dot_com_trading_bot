f56709e2c6f0e704b1d654199be52619111b073f
Dewmal <dewmalnilanka@gmail.com>
2019-12-15 12:47:03 +0530

    99 line code for binary trading

--- index.js
+++ index.js
@@ -1,80 +1,43 @@
 var ws = require('ws');
 const _ = require("lodash");
 var LiveApi = require('binary-live-api').LiveApi;
-
-var api = new LiveApi({ 
-    websocket: ws, 
-    appId: 0000//REPLACE_IT_WITH_YOURS
- });
-
-
+var api = new LiveApi({ websocket: ws, appId: 0000 });// Replace APP ID with your app id
 async function candleDetails(candle) {
-
-
     const open = candle.open;
     const high = candle.high;
     const low = candle.log;
     const close = candle.close;
-
     // Avoid dojo candles
     if (open === close || high === low) {
         return;
     }
-
     let type = '';
-
     if (open > close) {
         type = "BEARISH";
     } else if (open < close) {
         type = "BULLISH";
     }
-
     let bodySize = Math.abs(open - close);
     let upperBodySize = open > close ? high - open : high - close;
     let lowerBodySize = open > close ? close - low : open - low;
     let candleSize = high - low;
-
-    // Lets make this sensable 
-    // Aditional Step for you
-    let bodySizePer = bodySize * 100 / candleSize;
-    let upperBodySizePer = upperBodySize * 100 / candleSize;
-    let lowerBodySizePer = lowerBodySize * 100 / candleSize;
-
     return {
         "type": type,
         "bodySize": bodySize,
         "upperBodySize": upperBodySize,
-        "lowerBodySize": lowerBodySize,
-        "bodySizePer": bodySizePer,
-        "upperBodySizePer": upperBodySizePer,
-        "lowerBodySizePer": lowerBodySizePer,
+        "lowerBodySize": lowerBodySize
     }
 }
-
 async function identifyCandlePatternBullishEngulfing(candles) {
     candles = _.sortBy(candles, (ca) => ca.epoch); // Sort Candle List using Lodash
-
-
-    // Step 1
-    // Are we on down trend
     let direction = candles[0].close - candles[candles.length - 1].close;
     if (direction > 0) {
-        console.log("Trend is down");
-
         let firstCandle = candles[candles.length - 1];
         let secondCandle = candles[candles.length - 2];
-
-        // Step 2
-        // Second candle should be bearsh
         let secondCandleSummary = candleDetails(secondCandle);
         if (secondCandleSummary.type === "BEARISH") {
-
             let firstCandleSummary = candleDetails(firstCandle);
-            // Step 3
-            // Bullish Candle
             if (firstCandleSummary === "BULLISH") {
-                // Step 4                  
-                // Find engulfs               
                 if (firstCandle.bodySize > secondCandle.bodySize) {
                     console.log("yeah we found it");
                     return true;
@@ -84,23 +47,16 @@ async function identifyCandlePatternBullishEngulfing(candles) {
         return false;
     }
 }
-
-
-
 async function main() {
-    await api.authorize('REPLACE_IT_WITH_YOURS');
-
+    await api.authorize('YOUR_API_KEY');
     api.subscribeToBalance();
     api.events.on("balance", (res) => {
         const balance = res.balance;
         console.log(balance);
     });
-
     // Read market data
     let candles = [];
-
     api.getTickHistory("R_50", { end: 'latest', count: 5, "style": "candles", "subscribe": 1 });
-
     api.events.on("candles", async (res) => {
         candles = res.candles;
         identifyCandlePatternBullishEngulfing(candles);
@@ -117,12 +73,10 @@ async function main() {
                 low: ohlc.low,
                 close: ohlc.close,
             });
-
             // Remove Unwanted candles
             if (candles.length > 10) {
                 candles.pop();
             }
-
             if (identifyCandlePatternBullishEngulfing(candles)) {
                 // Place Order
                 let param = {};
@@ -132,9 +86,8 @@ async function main() {
                 param.currency = 'USD';
                 param.duration = 2;
                 param.duration_unit = 'm';
-                param.symbol = symbol;
+                param.symbol = "R_50";
                 let max = 1;
-
                 this.api.buyContractParams(param, max)
                     .then(console.log)
                     .catch(console.error);
@@ -142,17 +95,5 @@ async function main() {
             }
         }
     });
-
-    // Make Decision
-
-    // Withdraw profit
-
-
-
-
-
-
-
 }
-
 main().then(console.log).catch(console.error);
